9436f7cc8d60fb908f16e60053bdfd80a263e1fe
leivathan <andrewk3@illinois.edu>
2018-03-28 12:04:49 -0500

    Put in my key, what you guys want?

--- src/main/java/APITest.java
+++ src/main/java/APITest.java
@@ -16,7 +16,7 @@ public class APITest {
 
     /** Subscription key. Replace with your API key. */
     private static final String SUBSCRIPTION_KEY =
-            "13hc77781f7e4b19b5fcdd72a8df7156";
+            "219f27e97252416fb8788fd063ccc7e1";
 
     /**
      * Region to use.
